38f7393aa10d58503840f18c3fb563dad629b807
Arturo Jimenez <arturo.jimenez@freejolitos.com>
2026-04-24 01:12:47 -0600

    fix: log public/ contents after build

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ ZOLA_VERSION="0.19.2"
 curl -sL "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" \
   | tar xz -C /tmp
 /tmp/zola build
+echo "--- build output ---"
+ls -la public/
